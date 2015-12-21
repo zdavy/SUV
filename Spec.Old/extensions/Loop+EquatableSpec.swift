408107d9a8b88c73250be38c229ae62b5ad8d28a
Zachary Davy <zdavy@8thlight.com>
2015-12-21 16:33:12 +0000

    start refactoring specs, move old specs out of sources

---
+++ Spec.Old/extensions/Loop+EquatableSpec.swift
@@ -0,0 +1,17 @@
+import SUV
+import Swiftest
+
+class LoopEquatableSpec: Spec {
+    let spec = describe("Loop :: Equatable") {
+    it("equal when the pointers are the same") {
+      let pointer = UnsafeMutablePointer<UVLoopType>.alloc(sizeof(UVLoopType))
+      expect(Loop(pointer)).to.equal(Loop(pointer))
+    }
+
+    it("unequal when the pointers are different") {
+      let pointer1 = UnsafeMutablePointer<UVLoopType>.alloc(sizeof(UVLoopType))
+      let pointer2 = UnsafeMutablePointer<UVLoopType>.alloc(sizeof(UVLoopType))
+      expect(Loop(pointer1)).notTo.equal(Loop(pointer2))
+    }
+  }
+}
