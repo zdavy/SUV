3df9b9cda3b4f7ccd2bf93dc6e90d83b648b9da4
Zachary Davy <zdavy@8thlight.com>
2015-12-25 18:27:37 +0000

    bump Swiftest 0.1.4 -> 0.1.5

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "SUV",
     dependencies: [
       .Package(url: "https://github.com/zachmokahn/libUV.git", Version(1,0,0)),
-      .Package(url: "https://github.com/bppr/Swiftest.git", Version(0,1,4))],
+      .Package(url: "https://github.com/bppr/Swiftest.git", Version(0,1,5))],
     targets: [
       Target(name: "SUV"),
       Target(name: "Spec", dependencies: [.Target(name: "SUV")])
