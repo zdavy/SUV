78b9aacef294db2bfa01c53fbf741aa05d518f85
Zachary Davy <zdavy@8thlight.com>
2015-12-26 04:54:58 +0000

    bump libUV 1.0.0 > 2.0.0

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "SUV",
     dependencies: [
-      .Package(url: "https://github.com/zachmokahn/libUV.git", Version(1,0,0)),
+      .Package(url: "https://github.com/zachmokahn/libUV.git", Version(2,0,0)),
       .Package(url: "https://github.com/bppr/Swiftest.git", Version(0,1,5))],
     targets: [
       Target(name: "SUV"),
